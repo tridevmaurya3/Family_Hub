2bbf0a21392ae3128bce3528939f6c8a8745a96d
tridevmaurya3 <tridevmaurya3@gmail.com>
2026-07-23 20:46:17 +0530

    Apply Fluent design system to dashboard

--- app/src/main/java/com/tridev/familyhub/core/ui/cards/HeroCardView.java
+++ app/src/main/java/com/tridev/familyhub/core/ui/cards/HeroCardView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.material.button.MaterialButton;
 import com.tridev.familyhub.R;
 
 /**
@@ -22,7 +22,7 @@ public class HeroCardView extends FrameLayout {
     private ImageView heroIcon;
     private TextView heroTitle;
     private TextView heroSubtitle;
-    private Button heroActionButton;
+    private MaterialButton heroActionButton;
 
     private OnActionClickListener actionClickListener;
 
