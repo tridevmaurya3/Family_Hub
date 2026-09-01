a35b46f7d883d0fa9b503d5bbe1bdb51d530e992
tridevmaurya3 <tridevmaurya3@gmail.com>
2026-09-01 14:12:30 +0530

    Register safe Grocery professional UI binder

--- app/src/main/java/com/tridev/familyhub/core/FamilyHubApplication.java
+++ app/src/main/java/com/tridev/familyhub/core/FamilyHubApplication.java
@@ -13,6 +13,7 @@ import com.tridev.familyhub.feature.automation.FamilyAutomationLiveMonitor;
 import com.tridev.familyhub.feature.automation.FamilyAutomationRuntime;
 import com.tridev.familyhub.feature.automation.FamilyAutomationScheduler;
 import com.tridev.familyhub.feature.documents.DocumentExpiryScheduler;
+import com.tridev.familyhub.feature.grocery.GroceryProfessionalUiBinder;
 import com.tridev.familyhub.feature.grocery.GroceryRecurrenceEngine;
 import com.tridev.familyhub.feature.integration.MoneyManagerAccountAnalyticsBinder;
 import com.tridev.familyhub.feature.integration.MoneyManagerFinancePeriodBinder;
@@ -56,6 +57,7 @@ public class FamilyHubApplication extends Application {
 
         enableFirebaseOfflinePersistence();
         GroceryRecurrenceEngine.register(this);
+        GroceryProfessionalUiBinder.register(this);
         BackupScheduler.sync(this);
         DocumentExpiryScheduler.sync(this);
         registerActivityLifecycleCallbacks(new FamilyLivePrecisionActivityCallbacks());
