837ff52e78fbd9f4e46c1aaaf1a4aa8f514c83cd
tridevmaurya3 <tridevmaurya3@gmail.com>
2026-09-11 12:50:50 +0530

    Fix task accordion adapter import

--- app/src/main/java/com/tridev/familyhub/feature/tasks/FamilyTaskAdapter.java
+++ app/src/main/java/com/tridev/familyhub/feature/tasks/FamilyTaskAdapter.java
@@ -8,6 +8,7 @@ import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -252,4 +253,3 @@ final class FamilyTaskAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         }
     }
 }
-import android.widget.TextView;
